69040f623bced4f49c5c43f41e45df2700cd148b
Julian George <jules104@gmail.com>
2019-09-23 14:44:35 +0100

    Add signup form passing through information

--- server.rb
+++ server.rb
@@ -8,5 +8,17 @@ class MakersBNB < Sinatra::Base
   get '/' do
     erb :sign_up
   end
+
+  post '/user/new' do
+    p params.inspect
+    $firstname = params[:firstname]
+    redirect '/user'
+  end
+
+  get '/user' do
+    p $firstname
+    "Welcome #{$firstname}"
+  end
+
   run! if __FILE__ == $PROGRAM_NAME
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,3 +4,16 @@ feature "visiting homepage" do
     expect(page).to have_content("Please sign up")
   end
 end
+
+feature "Signing up" do
+  scenario "submits and directs new user information" do
+    visit('/')
+  fill_in 'email', with: 'abc@gmail.com'
+  fill_in 'username', with: 'abc'
+  fill_in 'firstname', with: 'abc'
+  fill_in 'lastname', with: 'xyz'
+  fill_in 'password', with: 'qwerty'
+  click_button 'submit'
+  expect(page).to have_content("Welcome abc")
+  end
+end
