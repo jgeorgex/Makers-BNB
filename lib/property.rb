ccdfb74ec6291220ba90150914e8e1027ea9d9db
Gabriel Gonzalez <gabokappa@gmail.com>
2019-09-23 17:46:52 +0100

    Find test failign awaiting passing green

--- lib/property.rb
+++ lib/property.rb
@@ -1,6 +1,6 @@
 class Property
 
-attr_reader :capacity
+attr_reader :id, :user_id, :address, :description, :capacity, :pricepn
 
   def initialize( id:, user_id:, address:, description:, capacity:, pricepn: )
     @id = id
@@ -18,7 +18,7 @@ attr_reader :capacity
         '#{description}', '#{capacity}', '#{pricepn}') RETURNING id, user_id,
         address, description, capacity, pricepn;").first
     Property.new(id: r['id'], user_id: r['user_id'], address: r['address'],
-    description: r['description'], capacity: r['capacity'],
+    description: r['description'], capacity: r['capacity'].to_i,
     pricepn: r['pricepn'])
   end
 end
--- spec/property_spec.rb
+++ spec/property_spec.rb
@@ -9,9 +9,22 @@ describe Property do
 
       listing = Property.create(user_id: user.id, address: '123 Mullholland Drive', description: 'Awesome shared bed',
       capacity: 4, pricepn: 100)
-      p listing
-      expect(listing.capacity).to eq("4")
-
+      expect(listing.capacity).to eq(4)
     end
   end
+
+  describe '#find' do
+    it 'finds the property on the database' do
+
+      user = MbnbUser.create(email: 'test@test.com', username: 'TestUserName',
+      firstname: 'John', surname: 'Bloggs', password: 'makers123')
+
+      listing = Property.create(user_id: user.id, address: '123 Mullholland Drive', description: 'Awesome shared bed',
+      capacity: 4, pricepn: 100)
+
+      expect(Property.find(listing.id)).to eq(listing)
+  
+    end
+  end
+
 end
