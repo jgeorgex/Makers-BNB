7f21980b549af1c653d119f067a379a5594d0254
Julian George <jules104@gmail.com>
2019-09-25 16:40:26 +0100

    Add select caleander select month function

--- src/calender_controller.js
+++ src/calender_controller.js
@@ -5,16 +5,10 @@ var events = [
   {'Date': new Date(2019, 8, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
   {'Date': new Date(2019, 8, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
 ];
-//
-// var yr = '2019'
-// var month = '9'  parse(yr, 10), parse(month, 10), parse( day, 10)
-// var day = '5'
-// // var newEvent = {'Date': new Date(2019, 8, 20)};
-//
-// events.push(newEvent)
 
-
-
+$("#caleandar").click(function(){
+alert("You have selected X ")
+})
 
 var settings = {
   DateTimeShow: true,
