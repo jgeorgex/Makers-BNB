fa296035e365bd71b193f38efa0a19237a7c0a13
Oliver Antcliff <antcliffoc@gmail.com>
2019-09-27 15:20:42 +0100

    bookings calendar displayed succesfully on the manage listings page

--- server.rb
+++ server.rb
@@ -22,6 +22,16 @@ class MakersBNB < Sinatra::Base
     json(y)
   end
 
+  get '/user/:id/reservations_api' do
+    @properties = Property.user_all(params[:id])
+    @reservations = []
+    @properties.each { |p| res = Reservation.all(property_id: p.id)
+      y = []
+      res.each { |date| y.push(date.gsub('-', ', '))}
+      @reservations << y }
+    json(@reservations)
+  end
+
   get '/login' do
     erb :login
   end
